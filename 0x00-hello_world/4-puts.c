182c3cd7a117161e88cf03c7c27aebaa85f286e2
25Emmanuel <ikwumonuemmanuel@gmail.com>
2022-03-14 02:45:25 -0700

    added c scripts that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+/*entering main
+ *
+ * returns 0 on success
+ */
 int main(void)
-{	
-	/*
-	 * main function
-	 *
-	 * always return 0 on success
-	 */
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
