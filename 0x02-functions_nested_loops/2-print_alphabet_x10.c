a25086e10e3a115a94617b89c98006ac9a7c9634
25Emmanuel <ikwumonuemmanuel@gmail.com>
2022-03-15 17:33:56 -0700

    a script that prints 10times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet
- *
- *
- * Return: Always (0)
- */
+* print_alphabet_x10 - Prints 10 times the alphabet
+*
+* Return: Always (0)
+*/
 void print_alphabet_x10(void)
 {
 int n, co;
