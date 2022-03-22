64199843fcfb9c46df1b63d2c288aec7f3fa6c79
25Emmanuel <ikwumonuemmanuel@gmail.com>
2022-03-21 19:09:27 -0700

    function that prints everyother char in a str

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 
 /**
- *  * puts2 - prints one char out of 2 of a string.
- *   * @str: input string.
- *    * Return: no return.
- *     */
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
+ */
 void puts2(char *str)
 {
 	int count = 0;
+
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
